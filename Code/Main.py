d22fe6b5d9ffda897fb0f64ec880f7d7b0801ede
Harrison Leece <harryleecemail@gmail.com>
2018-11-30 15:44:39 -0800

    WORKING CODE WOOOOOOOOOOOOOOOOOO

--- Code/Main.py
+++ Code/Main.py
@@ -5,6 +5,7 @@ from fricFracFxn import coleBrook
 from velocityFxn import velocityFxn
 from reyNumFxn import reyNum
 from scipy.optimize import fsolve
+import scipy.optimize as opt
 import numpy as np
 import matplotlib as plt
 
@@ -17,43 +18,64 @@ def equations(p, ffs):
 	ff2 = ffs[1]
 	ff3 = ffs[2]
 
-##	print('FF1: ' +str(ff1))
-##	print('FF2: ' +str(ff2))
-##	print('FF3: ' +str(ff3))
-##	print('Pump head: ' +str(Hp))
-##	print('v1: ' +str(v1))
-##	print('v2: ' + str(v2))
-##	print('v3: ' + str(v3))
+	print('FF1: ' +str(ff1))
+	print('FF2: ' +str(ff2))
+	print('FF3: ' +str(ff3))
+	print('Pump head: ' +str(Hp))
+	print('v1: ' + str(v1))
+	print('v2: ' + str(v2))
+	print('v3: ' + str(v3) + '\n')
+	
 	#Define constants
 	#Gravity m/s^2
 	g = 9.81
 	#density in kg/m^3
 	rho = 1000
 	#viscosity of water
-	mu = 8.4*10**(-4)
+	mu = 8.9*10**(-4)
 	#initial volumetric flow rate
 	Qin = .02120
 	#initial Pressure
 	P0 = 15000
 	p0g = P0/(g*rho)
 	#length
-	l1 = 36
-	l2 =35
-	l3 = 34
+	l0 = 40
+	l1 = 6
+	l2 = 4
+	l3 = 4
 	#Diameter 
 	D = 2/39.37
 	A1 = 3.1416/4 * D**2
 	A0 = 3.1416/4 * (4/39.37)**2
 	#initial flow velocity
 	v0 = Qin/A0
+	re0 = reyNum(v0,(4/39.37),rho,mu)
+	ff0 = coleBrook(.0000015/(4/39.37),re0)
+	#Major head loss
+	Hl = ff0*(l0/(4/39.37))*(v0**2)/(2*g)
+	#Roughness
+	e = 0.0000015
+	ed= e/D
 
-##	v1 = np.sqrt((2*g*(p0g+(v0**2/(2*g))+Hp-4))/(1+ff1*l1/D+.3))
-##	v2 = np.sqrt((2*g*(p0g+(v0**2/(2*g))+Hp-8))/(1+ff2*l2/D+.3))
-##	v3 = np.sqrt((2*g*(p0g+(v0**2/(2*g))+Hp-4))/(1+ff3*l3/D+.3))
-##      v1+v2+v3 = v0*A0/A1
+	print(A1)
+
+	nre1 = reyNum(v1,D,rho,mu)
+	nre2 = reyNum(v2,D,rho,mu)
+	nre3 = reyNum(v3,D,rho,mu)
+
+	ff1 = coleBrook(ed, nre1)
+	ff2 = coleBrook(ed, nre2)
+	ff3 = coleBrook(ed, nre3)
+
+	#f1 = ((2*g*((p0g+(v0**2/(2*g))+Hp - Hl -4)))/(1+ff1*l1/D +.3)) - v1**2
+	#f2 = ((2*g*((p0g+(v0**2/(2*g))+Hp - Hl -8)))/(1+ff2*l2/D +.3)) - v2**2
+	#f3 = ((2*g*((p0g+(v0**2/(2*g))+Hp - Hl -12)))/(1+ff3*l3/D +.3)) - v3**2
+	f1 = -(v1**2)/(2*g) * (1 + ff1*l1/D + 4.3) + p0g + v0**2/(2*g) + Hp - Hl - 4
+	f2 = -(v2**2)/(2*g) * (1 + ff2*l1/D + 3.3) + p0g + v0**2/(2*g) + Hp - Hl - 8
+	f3 = -(v3**2)/(2*g) * (1 + ff3*l1/D + 1.3) + p0g + v0**2/(2*g) + Hp - Hl - 12
+	f4 = -Qin/A1 + abs(v1) + abs(v2) + abs(v3) 
 	
-	return ( np.sqrt((2*g*(p0g+(v0**2/(2*g))+Hp-4))/(1+ff1*l1/D+.3)) -v1,np.sqrt((2*g*(p0g+(v0**2/(2*g))+Hp-8))/(1+ff2*l2/D+.3)) -v2,np.sqrt((2*g*(p0g+(v0**2/(2*g))+Hp-4))/(1+ff3*l3/D+.3)) -v3,v1 + v2 + v3 - (v0*A0/A1))
-
+	return f1,f2,f3,f4
 
 #Gravity m/s^2
 g = 9.81
@@ -97,7 +119,7 @@ minorLoss1 = 30
 
 #pipe rougness
 e = 0.0000015
-ed= e/d1
+ed= e/D
 
 #############################################################################
 
@@ -114,16 +136,17 @@ nre1 = 0
 nre2 = 0
 nre3 = 0
 nre4 = 0
-Hp = 30
-p = np.array([3, 3, 3, Hp])
+Hp = 16
+p = np.array([2,2,10, Hp])
 ffs = np.array([ff1, ff2, ff3])
 #Runs program with guessed friction factor until all changes in reynold numbers are small
-while( abs(re1-nre1)>30 or abs(re2-nre2)>30 or abs(re3-nre3)>30):
+while( abs(re1-nre1)>30 or abs(re2-nre2)>30 or abs(re3-nre3)>30 ):
+        
 	re1 = nre1
 	re2 = nre2
 	re3 = nre3
 	
-	v1, v2 ,v3, Hp = fsolve(equations,p,ffs)
+	v1,v2,v3,Hp = opt.fsolve(equations,p,ffs)
 	
 	nre1 = reyNum(v1,d1,rho,mu)
 	nre2 = reyNum(v2,d1,rho,mu)
@@ -138,3 +161,5 @@ while( abs(re1-nre1)>30 or abs(re2-nre2)>30 or abs(re3-nre3)>30):
 print('velocity 1: ' + str(v1))
 print('velocity 2: ' + str(v2))
 print('velocity 3: ' + str(v3))
+print('Required head of pump: ' + str(Hp))
+
